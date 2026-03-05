1c8e1b18817d0a128fccb4a6e20d4c250199b31c
Jflorez29-r <juniorflorezrivera3@gmail.com>
2026-03-05 09:34:18 -0500

    agregar docstrign
    Added comments to explain string splitting and segment counting.

--- countSegments.py
+++ countSegments.py
@@ -4,9 +4,9 @@ def countSegments(s: str) -> int:
     :rtype: int
     """
 
-    nueva_cadena = s.split()
-    return len(nueva_cadena)
+    nueva_cadena = s.split() # covertir la cadena en una lista de cadenas, elimana espacios
+    return len(nueva_cadena) # devolver la cantidad de segmentos
 
 
 # prueba
-print(countSegments("Hello, my name is John"))
+print(countSegments("Hello, my name is John")) # 5
